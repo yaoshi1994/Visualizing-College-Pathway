51f780c0489ba34b6bcd98e18a1279a002539c01
Kasper Yang <kasperyhr@gmail.com>
2019-03-02 12:20:31 -0500

    Add node_modules into .gitignore

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 sudo apt-get -y upgrade
-sudo apt-get install python3 python3-pip django-admin python-django
-pip3 install Django numpy pandas jupyter
+sudo apt-get install -y python3 python3-pip django-admin python-django
+sudo apt install -y npm
+pip3 install Django numpy pandas
