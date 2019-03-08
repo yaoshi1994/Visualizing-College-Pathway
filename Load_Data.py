abb99e0cfa3159e6f92f54a4d5e49d27d81c6e1b
Kasper Yang <kasperyhr@gmail.com>
2019-03-08 18:07:58 -0500

    format the backend

--- Load_Data.py
+++ Load_Data.py
@@ -5,19 +5,21 @@ from collections import defaultdict
 import json
 
 # DEFINE CONSTANTS
-STUDENT_ID 	= 'emplid_anon'
-MAJOR		= 'plan'
-TERM		= 'strm'
-COURSE_NAME	= 'course'
-COURSE_ID	= 'course_id'
-THRESHOLD	= 0.5
-SOURCE		= 'source'
-TARGET		= 'target'
-WEIGHT 		= 'weight'
-ID 			= 'id'
-NODES 		= 'nodes'
-LINKS 		= 'links'
-DATA_FILE 	= 'pathway_viz_example.csv'
+STUDENT_ID 		= 'emplid_anon'
+MAJOR			= 'plan'
+TERM			= 'strm'
+COURSE_NAME		= 'course'
+COURSE_ID		= 'course_id'
+THRESHOLD		= 0.5
+SOURCE			= 'source'
+TARGET			= 'target'
+WEIGHT 			= 'weight'
+ID 				= 'id'
+NODES 			= 'nodes'
+LINKS 			= 'links'
+DATA_FILE 		= 'pathway_viz_example.csv'
+SPLITTER 		= '-'
+MAX_TERM_DIST	= 1
 
 # DEFINE CLASSES
 ## DEFINE MAJOR
@@ -48,11 +50,11 @@ class Transcript:
 		self.c_id 	= c_id
 
 # DEINE SETS
-students 		= {} # KEY IS STUDNET ID
-majors 			= {} # KEY IS MAJOR ID
-majors_rev		= {} # KEY IS MAJOR NAME
-courses 		= {} # KEY IS COURSE ID
-transcripts 	= defaultdict(dict) # KEY IS STUDENT ID
+students 			= {} # KEY IS STUDNET ID
+majors 				= {} # KEY IS MAJOR ID
+majors_rev			= {} # KEY IS MAJOR NAME
+courses 			= {} # KEY IS COURSE ID
+transcripts 		= defaultdict(lambda: defaultdict(dict)) # FIRST KEY IS MAJOR SECOND KEY IS STUDENT ID
 
 # DEFINE FUNCTIONS
 def update_major(m_name):
@@ -84,11 +86,14 @@ def update_transcript(s_id, m_name, s_term, c_id, c_name):
 		update_student(s_id, m_name)
 	if c_id not in courses:
 		update_course(c_id, c_name)
-	if s_id not in transcripts:
-		transcripts[s_id] = {}
-	if s_term not in transcripts[s_id]:
-		transcripts[s_id][s_term] = []
-	transcripts[s_id][s_term].append(Transcript(s_id, s_term, c_id))
+	m_id = majors_rev[m_name].m_id
+	if m_id not in transcripts:
+		transcripts[m_id] = defaultdict(dict)
+	if s_id not in transcripts[m_id]:
+		transcripts[m_id][s_id] = {}
+	if s_term not in transcripts[m_id][s_id]:
+		transcripts[m_id][s_id][s_term] = []
+	transcripts[m_id][s_id][s_term].append(Transcript(s_id, s_term, c_id))
 
 def update(data):
 	s_id = data[STUDENT_ID]
@@ -116,15 +121,11 @@ for i in range(N):
 # print("semesters:", df.strm.unique())
 
 # WE NEED TO DEFINE THE EDGE BETWEEN TWO NODES FOR EACH MAJOR
-## TRANSCRIPT SPLIT BY MAJOR
-transcript_major = defaultdict(lambda: defaultdict(dict))
-for m in majors:
-	transcript_major[m] = defaultdict(dict)
-for s_id in transcripts:
-	transcript_major[students[s_id].m_id][s_id] = transcripts[s_id]
-
 ## FIND TERMS
 terms = np.sort(df.strm.unique())
+
+course_relation = defaultdict(dict)
+course_size = defaultdict(dict)
 
 ## DEFINE RELATIONS BETWEEN TWO CLASSES
 ### IF TWO COURSES ARE IN THE SAME SEMESTER, PUT INDEX 0
@@ -133,47 +134,54 @@ terms = np.sort(df.strm.unique())
 ### EVIOUS SEMESTER, PUT INDEX 2. PUT INDEX 3, OTHERWISE.
 ### THE KEY IS DEFINED AS LOWER_COURSE_ID-HIGHER_COURSE_
 ### ID AND THE VALUE OF IT IS THE ARRAY.
-course_relation = defaultdict(dict)
-course_size = defaultdict(dict)
-for m in transcript_major:
-	course_relation[m] = {}
-	course_size[m] = {}
-	# cnt = 0
-	for s_id in transcript_major[m]:
-		# cnt += 1
-		# if cnt % 10 == 1:
-		# 	print(cnt)
-		for t1 in transcript_major[m][s_id]:
-			for c1 in range(len(transcript_major[m][s_id][t1])):
-				if transcript_major[m][s_id][t1][c1].c_id not in course_size[m]:
-					course_size[m][transcript_major[m][s_id][t1][c1].c_id] = 0
-				course_size[m][transcript_major[m][s_id][t1][c1].c_id] += 1
-				for t2 in transcript_major[m][s_id]:
-					for c2 in range(len(transcript_major[m][s_id][t2])):
+def getKeyRelation(course_1, course_2):
+	s_c1_id = str(course_1.c_id)
+	s_c2_id = str(course_2.c_id)
+	if s_c1_id > s_c2_id:
+		return s_c2_id + SPLITTER + s_c1_id, True
+	else:
+		return s_c1_id + SPLITTER + s_c2_id, False
+
+def getIndex(term_1, term_2, reverse):
+	if reverse:
+		term_1, term_2 = term_2, term_1
+	if term_1 == term_2:
+		idx = 0
+	elif term_1 + MAX_TERM_DIST >= term_2 > term_1:
+		idx = 1
+	elif term_2 < term_1 <= term_2 + MAX_TERM_DIST:
+		idx = 2
+	else:
+		idx = 3
+	return idx
+
+def update_course_relation(course_1, course_2, m_id):
+	i1 = np.where(terms == course_1.s_term)[0] # GET INDEX OF TERM_1
+	i2 = np.where(terms == course_2.s_term)[0] # GET INDEX OF TERM_2
+	key_relation, reverse = getKeyRelation(course_1, course_2)
+	idx = getIndex(i1, i2, reverse)
+	if key_relation not in course_relation[m_id]:
+		course_relation[m_id][key_relation] = [0,0,0,0]
+	course_relation[m_id][key_relation][idx] += 1
+
+for m_id in transcripts:
+	course_relation[m_id] = {}
+	course_size[m_id] = {}
+	for s_id in transcripts[m_id]:
+		for t1 in transcripts[m_id][s_id]: # term_1
+			for c1 in range(len(transcripts[m_id][s_id][t1])): # course_1 in term_1
+				course_1 = transcripts[m_id][s_id][t1][c1]
+				if course_1.c_id not in course_size[m_id]:
+					course_size[m_id][course_1.c_id] = 0
+				course_size[m_id][course_1.c_id] += 1
+				for t2 in transcripts[m_id][s_id]: # term_2
+					for c2 in range(len(transcripts[m_id][s_id][t2])): # course_2 in term_2
+						course_2 = transcripts[m_id][s_id][t2][c2]
 						if c1 == c2 and t1 == t2:
 							continue
-						i1 = np.where(terms == transcript_major[m][s_id][t1][c1].s_term)[0]
-						i2 = np.where(terms == transcript_major[m][s_id][t2][c2].s_term)[0]
-						c_id1 = transcript_major[m][s_id][t1][c1].c_id
-						c_id2 = transcript_major[m][s_id][t2][c2].c_id
-						flag = False
-						if c1 > c2:
-							flag = True
-						if i1 == i2:
-							idx = 0
-						elif (i2 == i1 + 1 and flag) or (i1 == i2 + 1 and not flag):
-							idx = 1
-						elif (i1 == i2 + 1 and flag) or (i2 == i1 + 1 and not flag):
-							idx = 2
-						else:
-							idx = 3
-						if not flag:
-							string = str(c_id1) + '-' + str(c_id2)
-						else:
-							string = str(c_id2) + '-' + str(c_id1)
-						if string not in course_relation[m]:
-							course_relation[m][string] = [0,0,0,0]
-						course_relation[m][string][idx] += 1
+						update_course_relation(course_1, course_2, m_id)
+						
+
 ### DEFINE NODES
 nodes = {}
 for m in course_size:
@@ -201,7 +209,7 @@ for m in course_relation:
 		weight = edge_sum / total_sum
 		if weight < THRESHOLD:
 			continue
-		ccs = cc.split("-")
+		ccs = cc.split(SPLITTER)
 		if reverse:
 			src = int(ccs[1])
 			trg = int(ccs[0])
